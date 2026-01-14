41a7a885ff57edfafd87b88873d7e424380e6746
0xJiawei <141008990+0xJiawei@users.noreply.github.com>
2026-01-14 10:12:22 +0800

    Update __init__.py

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -2,14 +2,35 @@
 爬虫模块
 """
 from .base import BaseScraper, Job
-from .getro import GetroScraper, create_getro_scrapers
+from .getro import (
+    GreenhouseScraper,
+    AshbyScraper,
+    LeverScraper,
+    WorkableScraper,
+    create_getro_scrapers,
+    create_vc_portfolio_scrapers,
+)
 from .web3career import Web3CareerScraper, create_web3career_scraper
+
+
+def create_all_scrapers() -> list[BaseScraper]:
+    """创建所有爬虫"""
+    scrapers = []
+    scrapers.extend(create_vc_portfolio_scrapers())
+    scrapers.append(create_web3career_scraper())
+    return scrapers
+
 
 __all__ = [
     "BaseScraper",
     "Job",
-    "GetroScraper",
-    "create_getro_scrapers",
+    "GreenhouseScraper",
+    "AshbyScraper",
+    "LeverScraper",
+    "WorkableScraper",
     "Web3CareerScraper",
+    "create_getro_scrapers",
+    "create_vc_portfolio_scrapers",
     "create_web3career_scraper",
+    "create_all_scrapers",
 ]
